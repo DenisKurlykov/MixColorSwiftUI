a30cdea27dfcf3493b2d2803078bc8946aacfdbb
DenisKurlykov <dkurlykov@gmail.com>
2022-09-12 21:57:21 +0400

    Initial Commit

---
+++ MixColorSwiftUI/MixColorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MixColorSwiftUIApp.swift
+//  MixColorSwiftUI
+//
+//  Created by Денис Курлыков on 12.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct MixColorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
